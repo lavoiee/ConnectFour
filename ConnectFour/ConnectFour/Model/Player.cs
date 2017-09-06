3adb557614f7817629d35c00a371a3cf4c0006e2
Eric <ericjohnlavoie@gmail.com>
2017-09-05 20:05:47 -0400

    Fixed constructor in Player class

--- ConnectFour/ConnectFour/Model/Player.cs
+++ ConnectFour/ConnectFour/Model/Player.cs
@@ -28,6 +28,9 @@ namespace ConnectFour
         #endregion
 
         #region CONSTRUCTORS
+        public Player()
+        {
+        }
         public Player(string name)
         {
             _name = name;
