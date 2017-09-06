e244fb93d6d9ab605170cbf0aea177542dfff8ce
Eric <ericjohnlavoie@gmail.com>
2017-09-06 03:32:42 -0400

    Truncated namespaces in ConsoleUtilities classes.

--- ConnectFour/ConnectFour/ConsoleUtilities/ConsoleWindowHelper.cs
+++ ConnectFour/ConnectFour/ConsoleUtilities/ConsoleWindowHelper.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace ConnectFour.ConsoleUtilities
+namespace ConnectFour
 {
     class ConsoleWindowHelper
     {
